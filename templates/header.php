66c668253870d7ecf17c629b773157a2d43bde2b
Vladislav Grishak <grishakvlad@gmail.com>
2022-10-01 21:40:45 +0300

    add page-header

---
+++ templates/header.php
@@ -0,0 +1,25 @@
+<header class="page-header">
+    <div class="page-header__container container">
+        <div class="page-header__logo"><img src="/img/rfs-logo.svg" alt="RFS logo"></div>
+        <nav class="page-header__nav-menu">
+            <ul class="page-header__list">
+                <li class="page-header__item"><a href="/">Главная</a></li>
+                <li class="page-header__item"><a href="/#">Новости</a></li>
+                <li class="page-header__item page-header__sub-menu">
+                    <span>РПЛ</span>
+                    <ul class="page-header__sub-list">
+                        <li class="page-header__sub-item"><a href="#">Матчи</a></li>
+                        <li class="page-header__sub-item"><a href="#">Турнирная таблица</a></li>
+                        <li class="page-header__sub-item"><a href="#">Статистика</a></li>
+                    </ul>
+                </li>
+                <li class="page-header__item">Связаться с нами</li>
+            </ul>
+        </nav>
+        <div class="page-header__search-box">
+            <label>
+                <input class="page-header__search" type="text" placeholder="поиск...">
+            </label>
+        </div>
+    </div>
+</header>
